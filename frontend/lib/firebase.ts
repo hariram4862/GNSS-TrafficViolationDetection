09014ed5aa9f069d1b90002afdf797fc99b42754
Hari Ram <hariramreddy6204@gmail.com>
2025-02-28 12:05:37 +0530

    added map component

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, doc, getDoc,addDoc, collection,onSnapshot, getDocs, query, where } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4Imu4bB-JrIpiVIBmcOvtclVhyJqJE30",
-  authDomain: "traffic-violations-a8dc6.firebaseapp.com",
-  projectId: "traffic-violations-a8dc6",
-  storageBucket: "traffic-violations-a8dc6.firebasestorage.app",
-  messagingSenderId: "437718234145",
-  appId: "1:437718234145:web:cc73ffa6c9296737945f60",
-  measurementId: "G-C3S6X3WZ7J"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
